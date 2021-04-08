1e8468a075fb74349392f03bb8f040cce28b4f4a
Dell <sunkararam1166@gmail.com>
2021-04-09 02:39:49 +0530

    cvbcxv

--- src/XLApachePOI/WritingDataIntoXL.java
+++ src/XLApachePOI/WritingDataIntoXL.java
@@ -15,39 +15,39 @@ public class WritingDataIntoXL {
 
 	public static void main(String[] args) throws IOException {
 			
-			//FileInputStream fi= new FileInputStream("D:\\ReadDt.xlsx");
+			FileInputStream fi= new FileInputStream("D:\\ReadDt.xlsx");
 			
-//			Workbook wb = new XSSFWorkbook(fi);
-//			
-//			Sheet s1 = wb.getSheet("Love");
-//			
-//			Row r1,r2;
-//			
-//			Cell c1,c2;
-//			
-//			r1 = s1.getRow(1);
-//			
-//			c1 = r1.createCell(2);
-//			
-//			c1.setCellValue("Pass");
-//			
-//			
-//			r2 = s1.getRow(2);
-//			
-//			c2 = r2.createCell(3);
-//			
-//			c2.setCellValue("Fail");
-//			
-//			FileOutputStream fo = new FileOutputStream("D:\\ReadDt.xlsx");
-//			
-//			wb.write(fo);
-//			
-//			wb.close();
-//			
-//			fi.close();
-//			
-//			fo.close();
-// 		
+			Workbook wb = new XSSFWorkbook(fi);
+			
+			Sheet s1 = wb.getSheet("Love");
+			
+			Row r1,r2;
+			
+			Cell c1,c2;
+			
+			r1 = s1.getRow(1);
+			
+			c1 = r1.createCell(2);
+			
+			c1.setCellValue("Pass");
+			
+			
+			r2 = s1.getRow(2);
+			
+			c2 = r2.createCell(3);
+			
+			c2.setCellValue("Fail");
+			
+			FileOutputStream fo = new FileOutputStream("D:\\ReadDt.xlsx");
+			
+			wb.write(fo);
+			
+			wb.close();
+			
+			fi.close();
+			
+			fo.close();
+ 		
 	}
 
 }
